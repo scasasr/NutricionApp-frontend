24d2600f3f0ddf35d496762e82e57028983b2c40
scasasr <CORREO@example.com>
2023-06-08 20:32:45 -0500

    Tip FAB implementation

--- src/components/Tip.js
+++ src/components/Tip.js
@@ -1,26 +1,91 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+
 import { useAuth0 } from "@auth0/auth0-react";
+
+import API from "../services/http-common.js"
+
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
+import { Tooltip } from "@mui/material";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 import BoltIcon from '@mui/icons-material/Bolt';
 
-const FAB = () => {
-    const {user,isAuthenticated} = useAuth0();
-    const [showSnackbar,setShowSnackbar] = useState(false);
 
-    const handleShowSnackbar = () =>{
-        setShowSnackbar(true);
-        console.log(showSnackbar);
+
+const FAB = () => {
+
+    const Alert = React.forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+      });
+
+    const {user,isAuthenticated} = useAuth0();
+    const [showTips, setShowTips] = useState(false);
+    const [tip,setTip] = useState('');
+    const [tipData,setTipData] = useState([]);
+
+    const getUserId = async() =>{
+        await API.get('/user/correo/'+user.email).then((response)=>{
+            if(response.status === 200){
+                getGoalId(response.data.id_usuario);
+            }
+        }).catch((error) => {
+            console.log(error);
+        })
     }
+
+    const getGoalId = async (userId) =>{
+        await API.get('/rutinas/'+userId).then((response)=>{
+            if(response.status === 200){
+                getTipData(response.data[0].objetivo.id_objetivo);
+            }
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
+    const getTipData = async (goalId) =>{
+        await API.get('/tip/'+goalId).then((response)=>{
+            if(response.status === 200){
+                setTipData(response.data);
+            }
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+    
+
+    const toggleTips = () => {
+      setTip((tipData[Math.floor(Math.random() * tipData.length)]).tip)
+      setShowTips(true);
+      setTimeout(()=>setShowTips(false),4000);
+    };
+
+    useEffect(() => {
+        getUserId();
+    },);
+
+
 
     return (
         <>
         {isAuthenticated ?(
-            <Box sx={{position:"fixed",bottom:50 ,left:16 }}>
-                <Fab color="success" aria-label="Tip">
-                    <BoltIcon />
-                </Fab>
-            </Box>
+            <div>
+                <Box sx={{position:"fixed",bottom:50 ,left:16 }}>
+                    <Tooltip title='Tip objetivo'>
+                        <Fab color="success" aria-label="Tip" onClick={toggleTips}>
+                            <BoltIcon />
+                        </Fab>
+                    </Tooltip>
+                </Box>
+                <Snackbar  anchorOrigin={{vertical:'top', horizontal:'left'}} open={showTips} autoHideDuration={4000} onClose={() => setShowTips(false) }>
+                    <Alert onClose={() => setShowTips(false)} severity="success" sx={{ width: '100%' }}>
+                        {tip}
+                    </Alert>
+                </Snackbar>
+            </div>
+            
+            
             ):(<></>)}
         </>  
     );
